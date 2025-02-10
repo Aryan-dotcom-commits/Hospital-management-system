585b9f001c7a7657893f5e2f9108e6293b31c80f
Aryan <buggy.the.crown.1945@gmail.com>
2025-02-10 23:10:41 +0545

    changes in Models

--- backend/Models/UserModel.cs
+++ backend/Models/UserModel.cs
@@ -1,18 +1,18 @@
-using System.ComponentModel.DataAnnotations;
-using Models.Enums;
-namespace Models.UserModel;
+    using System.ComponentModel.DataAnnotations;
+    using Models.Enums;
+    namespace Models.UserModel;
 
 
-public class UserModel {
-    [Key]
-    public int userID {get; set;}
-    public string Username {get; set;} = string.Empty;
-    [Required]
-    public string Usermail {get; set;} = string.Empty;
-    public UserType userType {get; set;}
-    public string password {get; set;} = string.Empty;
-    public int userAge {get; set;}
-    public string userBloodGroup {get; set;}
-    public string userMedicalDiagnosis {get; set;}
-    public string userAddress {get; set;}
-}
+    public class UserModel {
+        [Key]
+        public int userID {get; set;}
+        public string Username {get; set;} = string.Empty;
+        [Required]
+        public string Usermail {get; set;} = string.Empty;
+        public UserType userType {get; set;}
+        public string password {get; set;} = string.Empty;
+        public int userAge {get; set;}
+        public string userBloodGroup {get; set;}
+        public string userMedicalDiagnosis {get; set;}
+        public string userAddress {get; set;}
+    }
